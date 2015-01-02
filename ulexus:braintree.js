acd73089772093ac3af0dc75c5a4850e75254bf4
Ulexus <ulexus@gmail.com>
2015-01-02 17:35:21 -0500

    Add more wrapped calls

--- ulexus:braintree.js
+++ ulexus:braintree.js
@@ -6,7 +6,7 @@ braintreeConnect = function(opts) {
       throw new Error("Invalid parameter; must be object with connection information");
    }
    return new Braintree.connect(opts);
-}
+};
 
 // Wrapper class for braintree, for convenience
 BraintreeClient = function(opts) {
@@ -16,29 +16,41 @@ BraintreeClient = function(opts) {
    this.client =  new Braintree.connect(opts);
 
    return this;
-}
+};
 
 // createSale makes a sale (new transaction)
 BraintreeClient.prototype.createSale = function(opts) {
    var wrapped = Metor.wrapAsync(this.client.transaction.sale,this.client.transaction);
    return wrapped(opts);
-}
+};
 
 // createCustomer makes a new customer
 BraintreeClient.prototype.createCustomer = function(opts) {
    var wrapped = Meteor.wrapAsync(this.client.customer.create,this.client.customer);
    return wrapped(opts);
-}
+};
+
+// getCustomer attempts to retrieve a customer by id
+BraintreeClient.prototype.getCustomer = function(id) {
+   var wrapped = Meteor.wrapAsync(this.client.customer.find,this.client.customer);
+   return wrapped(id);
+};
+
+// createCreditCard saves a new credit card
+BraintreeClient.prototype.createCreditCard = function(opts) {
+   var wrapped = Meteor.wrapAsync(this.client.creditCard.create,thisclient.creditCart);
+   return wrapped(opts);
+};
 
 // createSubscription makes a new customer subscription
 // (recurring payment)
 BraintreeClient.prototype.createSubscription = function(opts) {
    var wrapped = Meteor.wrapAsync(this.client.subscription.create,this.client.subscription);
    return wrapped(opts);
-}
+};
 
 // cancelSubscription cancels a customer's subscription
 BraintreeClient.prototype.cancelSubscription = function(opts) {
    var wrapped = Meteor.wrapAsync(this.client.subscription.cancel,this.client.subscription);
    return wrapped(opts);
-}
+};
