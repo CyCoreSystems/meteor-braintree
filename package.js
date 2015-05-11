129eff6c18aa7399f4857f0b2ecce740c29cab26
Ulexus <ulexus@gmail.com>
2015-05-10 22:44:03 -0400

    Version bump: 1.24.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ulexus:braintree',
   summary: 'Braintree client library access package for Meteor',
-  version: '1.22.1',
+  version: '1.24.0-rc1',
   git: 'https://github.com/CyCoreSystems/meteor-braintree.git'
 });
 
@@ -21,5 +21,5 @@ Package.onTest(function(api) {
 });
 
 Npm.depends({
-   braintree: '1.22.1'
+   braintree: '1.24.0'
 });
